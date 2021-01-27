ac7ea48a6d6b86d9199fb1441eb366c9cf7b1368
Caden Claussen <caden.claussen@gmail.com>
2021-01-26 16:26:55 -0800

    Finished editing Icarus code

--- icarus.py
+++ icarus.py
@@ -1,22 +1,12 @@
-# Icarus
-
-def icarus():
-	number = 4
-	while number != 100:
-		time = 50
-		while number != 1:
-			if number % 2 == 1:
-				number = number * 3 + 1
-			else:
-				number = number/2
-			time -= 1
-			number += 1
-			if time == 0:
-				return number
-
-number_in_100 = icarus()
-
-if number_in_100 != None:
-	print(number_in_100)
-else:
-	print("none")
+n = 3
+n = int(n)
+print(str(int(n)) + ": ", end="")
+while n != 1:
+	if n % 2 == 1:
+		n = (n*3+1)
+		print(str(int((n-1)/3)) + " * 3 + 1 --> " + str(int(n)) + " = ", end="")
+	if n % 2 == 0:
+		n = (n/2)
+		print(str(int(n*2)) + "/2 --> " + str(int(n)) + " = ", end="")
+print("1")
+print("\n")
